69d6e9c3e764a285ae955fb2d3c90c93e1db9def
Anderson Souza <andersonsouza007@live.com>
2023-09-08 08:28:39 -0300

    Card exercise

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./Components/Card";
+
+function App(){
+  return (
+    <div className="app">
+        <Card /> 
+    </div>
+  )
+}
+export default App;
---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./style.module.css";
+import posterImg from "../../assets/star-wars.webp";
+export default function Card(){
+    return (
+        <div className={styles.container}>
+            <img className={styles.poster} src={posterImg} alt="Start Wars Poster" />
+            <div>
+                <h2>Poster do filme Star Wars</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maxime voluptate atque ipsum qui corrupti tenetur aperiam a sunt dignissimos quisquam, modi doloremque! Blanditiis explicabo quam ipsam? Earum, ullam architecto.</p>
+                <button>Comprar agora</button>
+            </div>
+        </div>
+    )
+}
